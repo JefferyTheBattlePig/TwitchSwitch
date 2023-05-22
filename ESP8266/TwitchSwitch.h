4f6734e75d0d7d6e28a35e3ba2ebed10e13216d6
JeffreyPyg <35508608+JefferyTheBattlePig@users.noreply.github.com>
2023-05-21 22:03:00 -0400

    Create TwitchSwitch.h

---
+++ ESP8266/TwitchSwitch.h
@@ -0,0 +1,18 @@
+//---------------------------------------------------------
+// File       : TwitchIntegratedServo_Defs.h
+// Author     : JeffreyPyg (01-07-2023)
+// Purpose    : Definitions File for project wide constants
+//---------------------------------------------------------
+
+#ifndef ESP_8266_DEFS_H
+#define ESP_8266_DEFS_H
+
+// WiFi credentials
+//---------------------------------------------------------
+#define WIFI_SSID "YOUR_SSID"
+#define WIFI_PASS "YOUR_PW"
+#define WEBHOOK_PORT 3000
+#define D0 16
+
+
+#endif //ESP_8266_DEFS_H
