39b5cf1a73f9b460399f1e7b2d34dcaeacbefffa
JeffreyPyg <35508608+JefferyTheBattlePig@users.noreply.github.com>
2023-05-21 21:58:04 -0400

    Create TwitchSwitch.ino

---
+++ ESP8266/TwitchSwitch.ino
@@ -0,0 +1,83 @@
+//---------------------------------------------------------
+// File       : TwitchIntegratedServo.ino
+// Author     : JeffreyPyg (01-07-2023)
+// Purpose    : Integrate a MOSFET, to a twitch EventSub
+//---------------------------------------------------------
+
+#include <WiFiClientSecure.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include "TwitchIntegratedServo_Defs.h"
+
+WiFiServer wifiServer(3000);
+
+void setup() {
+
+  delay(1000);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_OFF);
+  delay(1000);
+  WiFi.mode(WIFI_STA);
+
+  // D0 on the board
+  pinMode(16, OUTPUT);
+
+  Serial.println();
+  Serial.println("Connecting to WiFi");
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(WIFI_SSID);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  wifiServer.begin();
+
+} // end setup()
+
+void loop() {
+
+  WiFiClient client = wifiServer.available();
+  
+  if (client) {
+    while (client.connected()) {
+ 
+      String payload = "";
+      bool isJSON = false;
+      while (client.available()>0) {
+        char c = client.read();
+        if (c == '{') {
+          isJSON = true;
+        }
+        if (isJSON){
+          payload += c;
+        }
+      }
+ 
+      delay(10);
+
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, payload);
+
+    String body = doc["body"];
+
+    if (body == "live"){
+      Serial.println("I just went live");
+        digitalWrite(16, HIGH);       // sets the digital D0 on
+        delay(10000);                  
+        digitalWrite(16, LOW);        // sets the digital D0 off
+        client.stop();
+    }
+    }
+
+    client.stop();
+    Serial.println("Client Disconnected");
+}
+}
